18f6a43a8729c89da0e4444b901c6e75c9ff445a
Christopher Sexton <github@codeography.com>
2016-05-26 00:13:43 -0400

    Remove xcpretty

--- lib/radius/toolbelt/xcode_helpers.rb
+++ lib/radius/toolbelt/xcode_helpers.rb
@@ -15,7 +15,7 @@ module Radius
       end
 
       def xcode(action, params)
-        system "xcodebuild #{params.map {|k,v| "-#{k} #{v}"}.join ' '} #{action} | xcpretty"
+        system "xcodebuild #{params.map {|k,v| "-#{k} #{v}"}.join ' '} #{action}"
       end
 
       def agvtool_version
